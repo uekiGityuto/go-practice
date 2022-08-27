c55a159cc89add5e8110d6285042069f8ea82a20
ueki-yuto <ueki-yuto@dmm.com>
2022-08-27 15:27:49 +0900

    middleware修正

--- ui/middleware/middleware.go
+++ ui/middleware/middleware.go
@@ -3,12 +3,18 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func Logger(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("[START] middleware")
+		start := time.Now()
+		path := r.URL.Path
+		method := r.Method
+		fmt.Printf("[start]time:%s, method:%s, path:%s\n", start.Format(time.RFC3339Nano), method, path)
 		next.ServeHTTP(w, r)
-		fmt.Println("[END] middleware")
+		end := time.Now()
+		elapsedTime := end.Sub(start).Milliseconds()
+		fmt.Printf("[END]time:%s, method:%s, path:%s, elapsed_time:%dms\n", end.Format(time.RFC3339Nano), method, path, elapsedTime)
 	}
 }
